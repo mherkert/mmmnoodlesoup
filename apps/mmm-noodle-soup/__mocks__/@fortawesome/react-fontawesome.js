a76e5aa3d42089029f48eec5ea4874a0e5125d07
mherkert <marion.herkert@gmx.de>
2025-04-19 00:18:00 +0200

    added fontawesome mock

---
+++ apps/mmm-noodle-soup/__mocks__/@fortawesome/react-fontawesome.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const FontAwesomeIcon = ({ icon, className }) => (
+  <span className={className} data-testid="font-awesome-icon" />
+);
--- apps/mmm-noodle-soup/src/components/duration/Duration.test.tsx
+++ apps/mmm-noodle-soup/src/components/duration/Duration.test.tsx
@@ -1,38 +1,37 @@
 import { render, screen } from "@testing-library/react";
 import { Duration } from "./Duration";
 import React from "react";
+
 describe("Duration", () => {
   it("duration renders correctly", () => {
     render(
       <Duration duration={{ preparation: 10, cooking: 10, waiting: 10 }} />
     );
+    expect(screen.getByTestId("font-awesome-icon")).toBeInTheDocument();
     expect(screen.getByText("30 min")).toBeInTheDocument();
   });
 
   it("duration renders correctly with empty duration", () => {
-    render(
-      <Duration duration={{}} />
-    );
+    render(<Duration duration={{}} />);
+    expect(screen.getByTestId("font-awesome-icon")).toBeInTheDocument();
     expect(screen.getByText("0 min")).toBeInTheDocument();
   });
 
   it("duration renders correctly with no waiting time", () => {
-    render(
-      <Duration duration={{ preparation: 10, cooking: 10 }} />
-    );
+    render(<Duration duration={{ preparation: 10, cooking: 10 }} />);
+    expect(screen.getByTestId("font-awesome-icon")).toBeInTheDocument();
     expect(screen.getByText("20 min")).toBeInTheDocument();
   });
 
   it("duration renders correctly with no cooking time", () => {
-    render(
-      <Duration duration={{ preparation: 10, waiting: 10 }} />
-    );
+    render(<Duration duration={{ preparation: 10, waiting: 10 }} />);
+    expect(screen.getByTestId("font-awesome-icon")).toBeInTheDocument();
     expect(screen.getByText("20 min")).toBeInTheDocument();
   });
 
   it("duration renders correctly with no preparation time", () => {
-    render(
-      <Duration duration={{ cooking: 10, waiting: 10 }} />
-    );
+    render(<Duration duration={{ cooking: 10, waiting: 10 }} />);
+    expect(screen.getByTestId("font-awesome-icon")).toBeInTheDocument();
+    expect(screen.getByText("20 min")).toBeInTheDocument();
   });
 });
