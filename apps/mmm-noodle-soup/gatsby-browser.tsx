f7e21c80921c99757da72e09bd71ec0e27356f55
mherkert <marion.herkert@gmx.de>
2025-06-21 15:42:59 +0200

    fixing layout shift due to fontawesome style

--- apps/mmm-noodle-soup/gatsby-browser.tsx
+++ apps/mmm-noodle-soup/gatsby-browser.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import Layout from "./src/components/Layout";
 import "./src/styles/global.css";
 import { Auth0Provider } from "@auth0/auth0-react";
+import { config } from "@fortawesome/fontawesome-svg-core";
+
+// Prevent FontAwesome from adding its CSS since we did it manually above
+config.autoAddCss = false;
 
 export const wrapPageElement = ({
   element,
@@ -10,6 +14,8 @@ export const wrapPageElement = ({
   element: React.ReactNode;
   props: React.HTMLAttributes<HTMLDivElement>;
 }) => {
+  console.log(process.env.GATSBY_AUTH0_DOMAIN);
+  console.log(process.env.GATSBY_AUTH0_CLIENT_ID);
   return (
     <Auth0Provider
       domain={process.env.GATSBY_AUTH0_DOMAIN!}
